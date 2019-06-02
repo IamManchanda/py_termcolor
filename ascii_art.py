92b63f89000ad9af70bed299068747a766c9e22e
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-06-02 11:59:29 +0530

    Improve code formatting!

--- ascii_art.py
+++ ascii_art.py
@@ -1,12 +1,16 @@
 from termcolor import colored
 from pyfiglet import figlet_format
 
+
 def ascii_art(message, color):
-    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+    valid_colors = ("red", "green", "yellow", "blue",
+                    "magenta", "cyan", "white")
     if color not in valid_colors:
-        print(f"As color {color} is not in valid color list, color will be printed in cyan")
+        print(
+            f"As color {color} is not in valid color list, color will be printed in cyan")
         color = "cyan"
-    return colored(figlet_format(message), color = color, attrs =["bold", "dark"])
+    return colored(figlet_format(message), color=color, attrs=["bold", "dark"])
+
 
 message = input("What would you like to print?\n")
 color = input("What color?\n").lower()
--- colors.py
+++ colors.py
@@ -1,4 +1,5 @@
 from termcolor import colored
 
-text = colored("Hi There!", color = "magenta", on_color = "on_cyan", attrs = ["blink"])
+text = colored("Hi There!", color="magenta",
+               on_color="on_cyan", attrs=["blink"])
 print(text)
